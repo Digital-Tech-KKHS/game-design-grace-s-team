bdb4a78d4b2cffc407e930683db0bd7f9f985f4d
Grace-McDonald <99299287+Grace-McDonald@users.noreply.github.com>
2023-05-18 12:14:22 +1200

    struggling with art and tile sizing

---
+++ MAIN/Trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Trees" tilewidth="128" tileheight="128" tilecount="32" columns="8">
+ <image source="Trees.png" width="1024" height="512"/>
+</tileset>
